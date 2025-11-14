27a498970719241f231453ae608d7fdfd5716af6
nomena <hei.nomenjanahary@gmail.com>
2025-11-14 12:17:30 +0300

    les reponses de la quatrième quéstion

--- Request.sql
+++ Request.sql
@@ -6,6 +6,7 @@ select e.id,e.first_name,e.last_name from employee e left join Leave l on e.id_e
 
 3-
 select e.first_name,e.last_name,l.id_leave,l.start_date,l.end_date,t.name from employee e inner join Leave l
-on e.id_employee=l.id_employee inner join Team t on e.id_Team=t.id_Team
+on e.id_employee=l.id_employee inner join Team t on e.id_Team=t.id_Team;
 
 4-
+select contract_type,count(*) from employee group by contract_type;
